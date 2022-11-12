b22b2dc21e319fafa5d16b8f3443405ff7f6b210
irealycode <irealycode@gmail.com>
2022-11-12 04:35:51 +0100

    Update pikadb.py

--- pikadb.py
+++ pikadb.py
@@ -71,133 +71,135 @@ $$/             made by : {Fore.YELLOW + 'irealycode'+ Fore.RESET}
 """]
 print(banner[random.randint(0,3)])
 
-def get_doc(table,doc):
-    #checks if doc exists and gets data
-    ret = {}
-    try:
-        db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
-        f = db_read.read()
-        db_read.close()
-        ret = json.loads(f)
-    except:
-        ret = 'none'
-    return ret
 
-def update_doc(table,doc,data):
-    #update or add items to doc
-    ret = {}
-    try:
-        db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
-        f = db_read.read()
-        db_read.close()
-        z = json.loads(f)
-        lst = list(data.keys())
-        for i in range(len(lst)):
-            z[lst[i]] = data[lst[i]]
-        db_write = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'w')
-        db_write.write(json.dumps(z))
-        ret = 'updated'
-    except:
-        ret = 'none'
-    return ret
+class PikaDB:
+    def get_doc(table,doc):
+        #checks if doc exists and gets data
+        ret = {}
+        try:
+            db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
+            f = db_read.read()
+            db_read.close()
+            ret = json.loads(f)
+        except:
+            ret = 'none'
+        return ret
 
-def add_doc(table,doc,data):
-    ret = {}
-    try:
-        db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
-        f = db_read.read()
-        db_read.close()
-        z = json.loads(f)
-        ret = 'already exists, maybe you meant update?'
-    except:
-        db_write = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'w')
-        db_write.write(json.dumps(data))
-        ret = 'added'
-    return ret
-
-def add_db(db_c):
-    ret = {}
-    try:
-        os.mkdir(f'{Path.home()}/.dbs/{db_c}/')
-        ret = 'db created'
-    except:
-        ret = 'already exists'
-    return ret
-
-def add_table(db_c,table):
-    ret = {}
-    try:
-        os.mkdir(f'{Path.home()}/.dbs/{db_c}/{table}/')
-        ret = 'table created'
-    except:
-        ret = 'already exists'
-    return ret
-
-def delete_doc(table,doc):
-    ret = ''
-    try:
-        db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
-        yn = input(f"are you sure you want to delete {doc} y/n? ")
-        if yn == "y":
+    def update_doc(table,doc,data):
+        #update or add items to doc
+        ret = {}
+        try:
+            db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
             f = db_read.read()
             db_read.close()
             z = json.loads(f)
-            os.remove(f'{Path.home()}/.dbs/{db}/{table}/{doc}')
-            ret = 'deleted'
-    except:
-        ret = 'doesnt exist'
-    return ret
+            lst = list(data.keys())
+            for i in range(len(lst)):
+                z[lst[i]] = data[lst[i]]
+            db_write = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'w')
+            db_write.write(json.dumps(z))
+            ret = 'updated'
+        except:
+            ret = 'none'
+        return ret
 
-def dump_table(table):
-    ret = {}
-    try:
-        x=os.listdir(f'{Path.home()}/.dbs/{db}/{table}/')
-        for i in range(len(x)):
-            db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{x[i]}', 'r')
+    def add_doc(table,doc,data):
+        ret = {}
+        try:
+            db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
             f = db_read.read()
+            db_read.close()
             z = json.loads(f)
-            ret[x[i]] = z
-    except:
-        ret = 'none'
-    return ret
+            ret = 'already exists, maybe you meant update?'
+        except:
+            db_write = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'w')
+            db_write.write(json.dumps(data))
+            ret = 'added'
+        return ret
 
-def list_dbs():
-    ret = {}
-    try:
-        x=os.listdir(f'{Path.home()}/.dbs/')
-        ret = x
-    except:
-        ret = 'none'
-    return ret
+    def add_db(db_c):
+        ret = {}
+        try:
+            os.mkdir(f'{Path.home()}/.dbs/{db_c}/')
+            ret = 'db created'
+        except:
+            ret = 'already exists'
+        return ret
 
-def db_exists(db):
-    ret = 1
-    try:
-        x=os.listdir(f'{Path.home()}/.dbs/')
-        for i in range(len(x)):
-            if x[i] == db:
-                ret *= 0
-    except:
+    def add_table(db_c,table):
+        ret = {}
+        try:
+            os.mkdir(f'{Path.home()}/.dbs/{db_c}/{table}/')
+            ret = 'table created'
+        except:
+            ret = 'already exists'
+        return ret
+
+    def delete_doc(table,doc):
+        ret = ''
+        try:
+            db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{doc}', 'r')
+            yn = input(f"are you sure you want to delete {doc} y/n? ")
+            if yn == "y":
+                f = db_read.read()
+                db_read.close()
+                z = json.loads(f)
+                os.remove(f'{Path.home()}/.dbs/{db}/{table}/{doc}')
+                ret = 'deleted'
+        except:
+            ret = 'doesnt exist'
+        return ret
+
+    def dump_table(table):
+        ret = {}
+        try:
+            x=os.listdir(f'{Path.home()}/.dbs/{db}/{table}/')
+            for i in range(len(x)):
+                db_read = open(f'{Path.home()}/.dbs/{db}/{table}/{x[i]}', 'r')
+                f = db_read.read()
+                z = json.loads(f)
+                ret[x[i]] = z
+        except:
+            ret = 'none'
+        return ret
+
+    def list_dbs():
+        ret = {}
+        try:
+            x=os.listdir(f'{Path.home()}/.dbs/')
+            ret = x
+        except:
+            ret = 'none'
+        return ret
+
+    def db_exists(db):
         ret = 1
-    return ret
+        try:
+            x=os.listdir(f'{Path.home()}/.dbs/')
+            for i in range(len(x)):
+                if x[i] == db:
+                    ret *= 0
+        except:
+            ret = 1
+        return ret
 
-def list_tables():
-    ret = {}
-    try:
-        x=os.listdir(f'{Path.home()}/.dbs/{db}/')
-        ret = x
-    except:
-        ret = 'none1'
-    return ret
+    def list_tables():
+        ret = {}
+        try:
+            x=os.listdir(f'{Path.home()}/.dbs/{db}/')
+            ret = x
+        except:
+            ret = 'none1'
+        return ret
 
-def list_docs(table):
-    ret = {}
-    try:
-        x=os.listdir(f'{Path.home()}/.dbs/{db}/{table}/')
-        ret = x
-    except:
-        ret = 'none'
-    return ret
+    def list_docs(table):
+        ret = {}
+        try:
+            x=os.listdir(f'{Path.home()}/.dbs/{db}/{table}/')
+            ret = x
+        except:
+            ret = 'none'
+        return ret
 
 commands = ["dbs","add","docs","use","dump","exit","help","delete","update","make"]
 special_characters = "'!@#$%^&*()-+?=,\<>/\""
@@ -205,10 +207,10 @@ while 1:
     try:
         command = input(f"pikadb[{Fore.GREEN + db + Fore.RESET}]> ")
         if command == "dbs":
-            print(list_dbs())
+            print(PikaDB.list_dbs())
         elif command == "tables":
             if db != '':
-                print(list_tables())
+                print(PikaDB.list_tables())
             else:
                 print("none")
         elif command.startswith("get "):
@@ -217,7 +219,7 @@ while 1:
                 table = cmd[3]
                 doc = cmd[1]
                 if table:
-                    print(get_doc(table,doc))
+                    print(PikaDB.get_doc(table,doc))
                 else:
                     print("table?")
             else:
@@ -231,7 +233,7 @@ while 1:
                 try:
                     data = json.loads(data)
                     if table:
-                        print(add_doc(table,doc,data))
+                        print(PikaDB.add_doc(table,doc,data))
                     else:
                         print("table?")
                 except:
@@ -247,7 +249,7 @@ while 1:
                 try:
                     data = json.loads(data)
                     if table:
-                        print(update_doc(table,doc,data))
+                        print(PikaDB.update_doc(table,doc,data))
                     else:
                         print("table?")
                 except:
@@ -260,7 +262,7 @@ while 1:
                 table = cmd[3]
                 doc = cmd[1]
                 if table:
-                    print(delete_doc(table,doc))
+                    print(PikaDB.delete_doc(table,doc))
                 else:
                     print("table?")
             else:
@@ -270,7 +272,7 @@ while 1:
             if db != '' and len(cmd) > 1 :
                 table = cmd[1]
                 if table:
-                    print(list_docs(table))
+                    print(PikaDB.list_docs(table))
                 else:
                     print("table?")
             else:
@@ -279,7 +281,7 @@ while 1:
             cmd = command.split()
             if len(cmd) > 1:
                 db_c = command.split()[1]
-                if db_exists(db_c)==0:
+                if PikaDB.db_exists(db_c)==0:
                     print(f"using {db_c}")
                     db = cmd[1]
                 else:
@@ -291,7 +293,7 @@ while 1:
             if db != '' and len(cmd) > 1 :
                 table_c = command.split()[1]
                 if table_c:
-                    print(dump_table(table_c))
+                    print(PikaDB.dump_table(table_c))
                 else:
                     print("table?")
             else:
@@ -302,13 +304,13 @@ while 1:
                 if cmd[1] == 'db' and not any(c in special_characters for c in cmd[2]):
                     table_db = cmd[2]
                     if table_db:
-                        print(add_db(table_db))
+                        print(PikaDB.add_db(table_db))
                     else:
                         print("table?")
                 elif cmd[1] == 'table' and not any(c in special_characters for c in cmd[2]):
                     table_db = cmd[2]
                     if db != '':
-                        print(add_table(db,table_db))
+                        print(PikaDB.add_table(db,table_db))
                 else:
                     print("you have to select either db or table \nyou cant have special characters except _")
                 

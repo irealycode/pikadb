de84b4705c5c851aac0039bdab60c2a3f67cf687
irealycode <irealycode@gmail.com>
2023-06-26 23:56:27 +0000

    Update setup.sh
    args

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ pip3 install -r requierments.txt
 mkdir ~/.dbs/
 mkdir ~/.pikadb/
 cp ./pikadb.py ~/.pikadb/
-echo "python3 ~/.pikadb/pikadb.py" > /bin/pikadb
+echo "python3 ~/.pikadb/pikadb.py $1 $2 $3" > /bin/pikadb
